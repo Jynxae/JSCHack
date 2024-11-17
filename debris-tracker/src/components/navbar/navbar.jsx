14488687b139d0806beccd8af50ea3c3e1bca612
Reem Alkhalily <realkhalily@hotmail.com>
2024-11-17 04:50:28 -0600

    Added login/signup pages

--- debris-tracker/src/components/navbar/navbar.jsx
+++ debris-tracker/src/components/navbar/navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import CarinaNebulaBanner from "../../assets/CarinaNebulaBanner.png";
 import Logo from "../../assets/LogoNoTitle.png";
 
 function Navbar() {
@@ -9,7 +8,6 @@ function Navbar() {
       <nav className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
         <div className="max-w-7xl mx-auto px-12 py-3 flex justify-between items-center">
           <div className="flex items-center space-x-2">
-            {" "}
             <img src={Logo} alt="logo" className="w-24"></img>
             <h1 className="text-xl font-bold text-gray-800">
               <Link to="/" className="text-gray-800 hover:text-gray-900">
@@ -18,9 +16,12 @@ function Navbar() {
             </h1>
           </div>
 
-          <ul className="flex space-x-24 justify-items-center pr-96">
+          <ul className="flex space-x-16">
             <li>
-              <Link to="/risk" className="text-gray-600 font-bold hover:text-gray-900">
+              <Link
+                to="/risk"
+                className="text-gray-600 font-bold hover:text-gray-900"
+              >
                 Risk Page
               </Link>
             </li>
@@ -41,6 +42,14 @@ function Navbar() {
               </Link>
             </li>
           </ul>
+
+          {/* Login Button */}
+          <Link
+            to="/login"
+            className="bg-gray-800 text-white font-bold py-2 px-6 rounded-md hover:bg-gray-900"
+          >
+            Log In
+          </Link>
         </div>
       </nav>
     </div>
